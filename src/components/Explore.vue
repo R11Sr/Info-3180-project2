21298001282674c46ed8dd9650b981b188bfee95
r11Sr <rs_burner2@outlook.com>
2022-04-15 01:26:30 -0500

    front for home,login completed, explore view search panel completed result in the form of cards to add.

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="container col-md-8">
+    <div class="container col-md-6 col-lg-6 col-xl-9 col-xxl-8">
         <div class="title">
             <h2>Explore</h2>
         </div>
 
-        <div class="d-flex">
-            <form action="##to do ##" method="GET">
-                <div class="form-group pb-3">
-                    <label for="make">Make:</label>
-                    <input type="text" name="make" class='form-control' id="make"  >
+        <div class="">
+            <form action="##to do ##" method="GET" class="d-flex p-2 align-items-end">
+                <div class="form-group pe-3">
+                    <label for="make">Make</label>
+                    <input type="text" name="make" class='form-control' id="make">
                 </div>
 
-                <div class="form-group pb-3">
-                    <label for="model">Model:</label>
-                    <input type="text" name="model" class='form-control' id="model" >
+                <div class="form-group pe-3">
+                    <label for="model">Model</label>
+                    <input type="text" name="model" class='form-control' id="model">
                 </div>
 
               <button type="submit" name="submit" class="btn btn-success" >Search</button>
@@ -23,3 +23,10 @@
 
     </div>
 </template>
+
+<style>
+button{
+    width: 10em;
+    height:2.5em;
+}
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,22 +3,25 @@
     
 <div class="container col-md-6">
 
-  <div class="login-form center-block text-white">
+  <div class="login-form container d-flex justify-content-center">
+    <div class="container ">
     <h2>Login to your account</h2>
-    <form action="##to do ##" class="pb-3 col-6" method="POST">
 
-      <div class="form-group pb-3">
-          <label for="email">Email:</label>
-          <input type="text" name="email" class='form-control' id="email"  placeholder="Enter your email">
-      </div>
+        <form action="##to do ##" class="p-3 col-md-6 col-lg-6 col-xl-9 col-xxl-8 container-outlined" method="POST">
 
-      <div class="form-group pb-3">
-          <label for="password">Password:</label>
-          <input type="password" name="password" class='form-control' id="password" >
+        <div class="form-group pb-3">
+            <label for="Username">Username</label>
+            <input type="text" name="Username" class='form-control' id="Username"  placeholder="Enter your Username">
+        </div>
 
-      </div>
-      <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:100%">Log in</button>
-    </form>
+        <div class="form-group pb-3">
+            <label for="password">Password</label>
+            <input type="password" name="password" class='form-control' id="password" >
+
+        </div>
+        <button type="submit" name="submit" class="btn btn-success btn-block" style="width:100%">Login</button>
+        </form>
+    </div>
   </div>
 </div>
 </template>
@@ -46,3 +49,13 @@ export default{
     }
 }
 </script>
+
+<style>
+.container-outlined{
+    /* border-style: solid; */
+    /* border-radius: 1em; */
+    /* border-width: .15em; */
+    /* border-color: gray; */
+     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+</style>
